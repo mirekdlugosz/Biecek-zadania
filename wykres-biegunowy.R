d14a283c9c14bf948e71ec18936c9e6722305c06
Mirosław Zalewski <miniopl@gmail.com>
2014-04-14 19:41:43 +0200

    wykres-biegunowy.R - przepisanie na funkcję

--- wykres-biegunowy.R
+++ wykres-biegunowy.R
@@ -1,13 +1,14 @@
 ## @knitr wykres.biegunowy1.11
-helper.t <- seq(0, 90, length.out=1000)
-cs <- c(1, 0.1, 2.2)
 
-for (i in seq_along(cs)) {
-  plot(-2:2, -2:2, type="n", xlab="", ylab="",
-       main=paste("Wykres funkcji w układzie biegunowym dla c =", cs[i]))
-  lines(
-    (1+sin(helper.t)) * cos(cs[i]*helper.t),
-    (1+sin(helper.t)) * sin(cs[i]*helper.t),
-    type="l"
-  )
+plot.polar <- function(c, limit=100) {
+  t <- seq(0, limit, by=0.01)
+  r <- 1 + sin(t)
+  fi <- c * t
+  x <- r * cos(fi)
+  y <- r * sin(fi)
+  
+  plot(x, y, type="l", xlab="", ylab="",
+       main=paste("Wykres funkcji w układzie biegunowym dla c = ", c))
 }
+
+lapply(c(1, 0.1, 2.2), plot.polar)
